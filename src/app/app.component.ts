676ff67fcf851e883444bdba188eb0ac6602cf8d
ravulapr <ravulapr@ebrd.com>
2021-03-18 19:24:59 +0000

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import * as ace from 'ace-builds';
+import {CONSTANTS} from '../constants';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'json-formatter';
+export class AppComponent implements AfterViewInit {
+    @ViewChild('editorElm') private editorElm: ElementRef<HTMLElement>;
+    editor: any;
+    isBeautified = false;
+    errorToBeautify = false;
+    isCopied = false;
+
+    ngAfterViewInit(): void {
+        ace.config.set('fontSize', '16px');
+        ace.config.set('basePath', CONSTANTS.basePath);
+        this.editor = ace.edit(this.editorElm.nativeElement);
+        this.editor.setOptions({
+            theme: 'ace/theme/github',
+            mode: 'ace/mode/json',
+            useWrapMode: false,
+            tabSize: 4,
+            wrap: true
+        });
+        this.editor.on('change', (data) => {
+            this.errorToBeautify = false;
+            this.isBeautified = false;
+            this.isCopied = false;
+        });
+    }
+
+    beautify(): void {
+        try {
+            const actualValue: string = this.editor.getValue();
+            this.editor.setValue(JSON.stringify(JSON.parse(actualValue), null, 4));
+            this.isBeautified = true;
+        } catch (e) {
+            this.errorToBeautify = true;
+        }
+    }
+
+    copyToClipboard(): void {
+        this.editor.selectAll();
+        this.editor.focus();
+        document.execCommand('copy');
+        this.isCopied = true;
+    }
 }
---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+const constants = {
+  basePath: 'https://unpkg.com/ace-builds@1.4.12/src-noconflict'
+}
+
+export const CONSTANTS = constants;
